a7004108d9909763da9bed1568ccbe81b798c816
Elektropay <richard.r@everpayinc.com>
2020-09-02 23:09:10 -0500

    Update DefaultHeader.tsx

--- frontend/components/Header/DefaultHeader/DefaultHeader.tsx
+++ frontend/components/Header/DefaultHeader/DefaultHeader.tsx
@@ -4,7 +4,6 @@ import Badge from '../../UiElements/Badge/Badge';
 import Logo from '../../UiElements/Logo/Logo';
 import SvgIcon from '../../UiElements/SvgIcon/SvgIcon';
 import Container from '../../UiElements/Container/Container';
-import TopMenu from '../HeaderMenu/TopMenu/TopMenu';
 import MainMenu from '../HeaderMenu/MainMenu/MainMenu';
 import AvatarMenu from '../HeaderMenu/AvatarMenu/AvatarMenu';
 import { useCartState } from '../../../contexts/cart/cart.provider';
@@ -33,7 +32,7 @@ const DefaultHeader: React.FC<{}> = () => {
 						}
 					/>
 					<MenuRight className="menu-right">
-						<TopMenu />
+						
 
 						<Badge
 							path="/shop/checkout"
